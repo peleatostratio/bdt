df4ae39f8bd62aed5beb1ecc25dc3af0f1309d9b
adoblas <adoblas@stratio.com>
2017-03-23 14:12:09 +0100

    fix selenium tests

--- src/test/java/com/stratio/qa/specs/HookGIT.java
+++ src/test/java/com/stratio/qa/specs/HookGIT.java
@@ -16,14 +16,22 @@
 package com.stratio.qa.specs;
 
 import com.stratio.qa.cucumber.testng.CucumberRunner;
-import com.stratio.qa.utils.BaseGTest;
+import com.stratio.qa.data.BrowsersDataProvider;
+
+import com.stratio.qa.utils.BaseTest;
 import cucumber.api.CucumberOptions;
+import org.testng.annotations.Factory;
 import org.testng.annotations.Test;
 
 @CucumberOptions(features = {"src/test/resources/features/seleniumIT.feature"})
-public class HookGIT extends BaseGTest {
+public class HookGIT extends BaseTest {
 
-    @Test
+    @Factory(enabled = false, dataProviderClass = BrowsersDataProvider.class, dataProvider = "availableUniqueBrowsers")
+    public HookGIT(String browser) {
+        this.browser = browser;
+    }
+
+    @Test(enabled = true)
     public void seleniumTest() throws Exception {
         new CucumberRunner(this.getClass()).runCukes();
     }
